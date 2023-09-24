9a58cf966208f318b3acd71d2ef2e05aa538329d
六六 <hexsixology@gmail.com>
2023-09-24 04:38:26 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,24 @@ fn init_configs() -> config::Config {
 }
 
 fn init_logger(log_level: &str) {
+    let mut wrong_log_level = false;
     pretty_env_logger::formatted_builder()
         .filter_level(match log_level {
+            "trace" => LevelFilter::Trace,
             "debug" => LevelFilter::Debug,
             "info" => LevelFilter::Info,
             "warn" => LevelFilter::Warn,
             "error" => LevelFilter::Error,
             _ => {
-                panic!("Wrong log_level, check your configs.toml.")
+                wrong_log_level = true;
+                LevelFilter::Info
             }
         })
         .init();
+    if wrong_log_level {
+        warn!("Wrong log_level, please check your configs.toml.");
+        info!("Set log_level to info as default.");
+    }
 }
 
 fn init() -> config::Config {
@@ -40,6 +47,23 @@ fn init() -> config::Config {
 async fn main() {
     let configs = init();
     if let Ok(body) = yandere::request(&configs.yandere.rss_url).await {
-        yandere::parse_pop_recent(&body);
+        let posts = yandere::parse_pop_recent(&body);
+        info!("{} posts in total", posts.len());
+        for (i, post) in posts.iter().enumerate() {
+            info!("{} of {} is now processing.", i, posts.len());
+            if post.score_filter(configs.yandere.score_threshold) {
+                info!("post({}) filtered because of low score", post.get_id());
+                continue;
+            }
+            // todo: already sent
+            if let Ok(parent_id) = post.get_parent() {
+                if let Ok(parent) = yandere::Post::new(parent_id).await {
+                    let children = parent.get_children().await;
+                    // todo: send children
+                }
+            } else {
+                // todo: send post
+            }
+        }
     }
 }
