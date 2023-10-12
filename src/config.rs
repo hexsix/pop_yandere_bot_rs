04ad09c78708c3146cc0890ec6bc007d2605b68d
六六 <hexsixology@gmail.com>
2023-10-12 06:40:19 +0000

    Make some of the configs optional

--- src/config.rs
+++ src/config.rs
@@ -22,14 +22,29 @@ impl Config {
 
 #[derive(Debug, Deserialize)]
 pub struct Core {
+    #[serde(default = "log_level_default")]
     pub log_level: String,
+    #[serde(default = "scheduler_default")]
     pub scheduler: String,
+}
+
+fn log_level_default() -> String {
+    String::from("info")
+}
+
+fn scheduler_default() -> String {
+    String::from("0 0 0,9,12,15,18,21 * * *")
 }
 
 #[derive(Deserialize)]
 pub struct Database {
     pub database_url: String,
+    #[serde(default = "expire_default")]
     pub expire: usize,
+}
+
+fn expire_default() -> usize {
+    7776000
 }
 
 impl fmt::Debug for Database {
@@ -60,9 +75,24 @@ impl fmt::Debug for Telegram {
 
 #[derive(Debug, Deserialize)]
 pub struct Yandere {
+    #[serde(default = "rss_url_default")]
     pub rss_url: String,
+    #[serde(default = "score_threshold_default")]
     pub score_threshold: i32,
+    #[serde(default = "updated_resend_default")]
     pub updated_resend: bool,
+}
+
+fn rss_url_default() -> String {
+    String::from("https://yande.re/post/popular_recent")
+}
+
+fn score_threshold_default() -> i32 {
+    0
+}
+
+fn updated_resend_default() -> bool {
+    false
 }
 
 #[cfg(test)]
