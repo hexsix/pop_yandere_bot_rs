6fcbea661441dd2e7613eeec968c9a7b0c72467e
hexsix <hexsixology@gmail.com>
2025-09-09 21:17:37 +0800

    feat: redis客户端

---
+++ app/services/redis/client.py
@@ -0,0 +1,39 @@
+from loguru import logger
+
+import redis.asyncio as redis
+from app.configs import RedisConfig
+
+
+class RedisClient:
+    def __init__(self, config: RedisConfig):
+        pool = redis.ConnectionPool.from_url(config.url)
+        self.client = redis.Redis.from_pool(pool)
+        self.expiration_time = 60 * 60 * 24 * 30  # 30 days
+
+    async def ping(self):
+        logger.debug("Pinging Redis")
+        return await self.client.ping()
+
+    async def close(self):
+        logger.debug("Closing Redis connection")
+        await self.client.aclose()
+
+    async def already_sent_post(self, post_id: str) -> bool:
+        logger.debug(f"Checking if post {post_id} has already been sent")
+        return await self.client.get(f"post:{post_id}") is not None
+
+    async def already_sent_posts(self, posts_ids: list[str]) -> bool:
+        logger.debug(f"Checking if posts {posts_ids} have already been sent")
+        for post_id in posts_ids:
+            if not await self.already_sent_post(post_id):
+                return False
+        return True
+
+    async def set_post_as_sent(self, post_id: str):
+        logger.debug(f"Setting post {post_id} as sent")
+        await self.client.set(f"post:{post_id}", "1", ex=self.expiration_time)
+
+    async def set_posts_as_sent(self, posts_ids: list[str]):
+        logger.debug(f"Setting posts {posts_ids} as sent")
+        for post_id in posts_ids:
+            await self.client.set(f"post:{post_id}", "1", ex=self.expiration_time)
