6826e3d12c98bf9e78d555c7c5c50c7f63d7fc84
hexsix <hexsixology@gmail.com>
2025-09-10 17:12:25 +0800

    test: redis client

--- app/services/redis/client.py
+++ app/services/redis/client.py
@@ -30,10 +30,34 @@ class RedisClient:
         return True
 
     async def set_post_as_sent(self, post_id: int):
-        logger.debug(f"Setting post {post_id} as sent")
-        await self.client.set(f"post:{post_id}", "1", ex=self.expiration_time)
+        try:
+            logger.debug(f"Setting post {post_id} as sent")
+            await self.client.set(f"post:{post_id}", "1", ex=self.expiration_time)
+        except Exception as e:
+            logger.error(f"Error setting post: {e}")
 
     async def set_posts_as_sent(self, posts_ids: list[int]):
-        logger.debug(f"Setting posts {posts_ids} as sent")
-        for post_id in posts_ids:
-            await self.client.set(f"post:{post_id}", "1", ex=self.expiration_time)
+        try:
+            logger.debug(f"Setting posts {posts_ids} as sent")
+            async with self.client.pipeline() as pipe:
+                for post_id in posts_ids:
+                    await pipe.set(f"post:{post_id}", "1", ex=self.expiration_time)
+                await pipe.execute()
+        except Exception as e:
+            logger.error(f"Error setting posts: {e}")
+
+
+async def test_already_sent():
+    post_ids = [1242294, 1242295]
+    client = RedisClient(RedisConfig())
+    logger.info(await client.ping())
+    await client.set_posts_as_sent(post_ids)
+    result = await client.already_sent_posts(post_ids)
+    assert result == True
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(test_already_sent())
+
